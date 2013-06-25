9fc6d56e2fe796886d83ef96f67f48690e867293
lamoustache <moustache@riseup.net>
2013-06-25 10:42:16 +0100

    redirect output and errors to /dev/null

--- ciscotclsigner.sh
+++ ciscotclsigner.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MSG="\033[32m"
+INFO="\033[32m"
 ERROR="\033[31m"
 TRACE="\033[33m"
 CLEAR="\033[0m"
@@ -70,91 +70,109 @@ do
 done
 
 
-create_ca(){
-_log "\n$TRACE DEBUG: Issuing OpenSSL command $CLEAR"
+gen_cert(){
+_log "\n$TRACE DEBUG:$CLEAR Issuing OpenSSL command"
 openssl req -x509 -newkey rsa:2048 -days 1095 -nodes -keyout $PRIVATE_KEY -out \
-    $CERT -subj /C=UK/O=IT/CN=IT
+    $CERT -subj /C=UK/O=IT/CN=IT > /dev/null 2>&1
 
-_log "\n$TRACE DEBUG: Checking error code from OpenSSL command $CLEAR\n"
 if [ $? -eq 0 ]; then
-    echo "$MSG Generated $PRIVATE_KEY private key and $CERT certificate\n"
+    echo "$INFO INFO:$CLEAR Generated $PRIVATE_KEY private key and $CERT certificate\n"
 else
-    echo "\n$ERROR ERROR: generating certificate\n"
+    echo "\n$ERROR ERROR:$CLEAR  certificate not created\n"
     exit 1
 fi
 }
 
 sign(){
-echo "\n$MSG SIGN mode enabled $CLEAR"
-_log "\n$TRACE DEBUG: Issuing OpenSSL command $CLEAR"
-openssl smime -sign -in $FILE -out $FILE.pk7 -signer $CERT -inkey \
-    $PRIVATE_KEY -outform DER -binary > /dev/null
+echo "\n$INFO >>>>> SIGN mode enabled <<<<< \n$CLEAR"
 
-_log "$TRACE DEBUG: Checking error code from OpenSSL command $CLEAR"
-if [ $? -eq 0 ]
-then
-    _log "$TRACE DEBUG: $FILE signed as PKCS7 binary file $CLEAR"
-    _log "$TRACE DEBUG: Processing Cisco header in $FILE.pk7 $CLEAR"
+_log "\n$TRACE DEBUG:$CLEAR Issuing OpenSSL command"
+
+openssl smime -sign -in $FILE -out $FILE.pk7 -signer $CERT -inkey \
+    $PRIVATE_KEY -outform DER -binary >/dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+    _log "$TRACE DEBUG:$CLEAR $FILE signed as PKCS7 binary file"
+    _log "$TRACE DEBUG:$CLEAR Processing Cisco header, converting binary to hex $FILE.pk7"
     xxd -ps $FILE.pk7 > $FILE.hex
-    awk 'NR==1{print "#Cisco Tcl Signature V1.0"}1 {printf "#"; print}' \
-    $FILE.hex > $FILE.hex_sig
-    echo "\n$MSG Create signed Tcl file $CLEAR\n"
-    cat $FILE $FILE.hex_sig > $FILE'_signed.tcl'
+    if [ $? -ne 0 ]; then
+        echo "$ERROR ERROR:$CLEAR Cisco header not processed, error during hex convertion"
+        exit 1
+    else
+        awk 'NR==1{print "#Cisco Tcl Signature V1.0"}1 {printf "#"; print}' \
+        $FILE.hex > $FILE.hex_sig
+        if [ $? -ne 0 ]; then
+            echo "$ERROR ERROR:$CLEAR Cisco header not processed, couldn't append Cisco header to signed file"
+            exit 1
+        else
+            echo "$INFO INFO:$CLEAR Cisco header append to sign Tcl file"
+            cat $FILE $FILE.hex_sig > $FILE'_signed.tcl'
+            if [ $? -ne 0 ]; then
+                echo "$ERROR ERROR:$CLEAR Couldn't create signed Tcl file"
+                exit 1
+            else
+                echo "$INFO INFO:$CLEAR Create signed Tcl file $FILE"
+                return 0
+            fi
+        fi
+    fi
 else
-    echo "$ERROR: Error signing files"
-    rm "$FILE.pk7"
-    exit 1
+    echo "\n$ERROR ERROR:$CLEAR file not signed\n"
+    if [ -z $FILE ]; then
+        rm "$FILE.pk7"
+        exit 1
+    else
+        exit 1
+    fi
 fi
 }
 
 verify(){
-echo "\n$MSG VERIFY mode enabled $CLEAR"
+echo "\n$INFO >>>>> VERIFY mode enabled <<<<< \n$CLEAR"
 
-_log "\n$TRACE DEBUG: Issuing OpenSSL command $CLEAR"
+_log "\n$TRACE DEBUG:$CLEAR Issuing OpenSSL command"
 
 openssl smime -verify -in $FILE.pk7 -CAfile $CERT -inform DER -content \
-    $FILE > /dev/null
+    $FILE >/dev/null 2>&1
 
-_log "$TRACE DEBUG: Checking error code from OpenSSL command $CLEAR"
-if [ $? -eq 0 ]
-then
-    echo "$MSG $FILE has been succesfully signed $CLEAR\n"
+if [ "$?" -eq 0 ]; then
+    echo "$INFO INFO:$CLEAR $FILE has been succesfully signed\n"
 else
-    echo "$ERROR $FILE is not correctly signed $CLEAR\n"
+    echo "$ERROR ERROR:$CLEAR $FILE not signed\n"
 fi
 }
 
 # check the options passed in parametre of the command.
 # If no mode selected we quit
 
-if [ -z "$MODE" ] 
-then
+if [ -z "$MODE" ]; then
+    echo "$ERROR ERROR:$CLEAR Select an operation mode\n"
     usage
-    echo "$ERROR Please select an operation mode\n"
     exit 1
 
 # if selected mode is "sign" we want path to certificate and private key
-elif [ "$MODE" = "sign" ]
-then
+elif [ "$MODE" = "sign" ]; then
     # if we don't have a file, a cert and a private key we exit and display the help
-    if [ -z $FILE ] || [ -z $CERT ] || [ -z $PRIVATE_KEY ]
-    then
-        echo "$ERROR Please specify a file to sign, the certificate and private key"
+    if [ -z $FILE ] || [ -z $CERT ] || [ -z $PRIVATE_KEY ]; then
+        echo "\n$ERROR ERROR:$CLEAR  Specify a file to sign, the certificate and private key\n"
         usage
         exit 1
     # otherwise we sign the file and verify the signature is correct
     else
         sign
-        verify
+        if [ $? -eq 0 ]; then
+            verify
+        else
+            "$ERROR ERROR:$CLEAR Tcl file not signed"
+            exit 1
+        fi
     fi
 
-elif [ "$MODE" = "verify" ]
-then
+elif [ "$MODE" = "verify" ]; then
     # if we don't have a cert we exit and display the help
-    if [ -z $FILE ] || [ -z $CERT ]
-    then
-        usage
-        echo "$ERROR Please specify a file to verify and the certificate\n"
+    if [ -z $FILE ] || [ -z $CERT ]; then
+        echo "\n$ERROR ERROR:$CLEAR Specify a file to verify and the certificate\n"
+        usage 
         exit 1
     # otherwise we verify the signature is correct
     else
@@ -162,19 +180,17 @@ then
     fi
          
 # if selected mode is "genkey" we generate keys and Ca cert        
-elif [ "$MODE" = "gencrt" ]
-then
-    if [ -z $CERT ] || [ -z $PRIVATE_KEY ]
-    then
+elif [ "$MODE" = "gencrt" ]; then
+    if [ -z $CERT ] || [ -z $PRIVATE_KEY ]; then
+        echo "\n$ERROR ERROR:$CLEAR Specify certificate and private key name\n"
         usage
-        echo "$ERROR Specify certificate and private key name\n"
         exit 1
     else
-        create_ca
+        gen_cert
     fi
 
 else
+    echo "$ERROR ERROR:$CLEAR Specify the operation mode <sign> | <verify> | <gencrt>\n"
     usage
-    echo "$ERROR Specify a mode <sign> | <verify> | <gencrt>\n"
     exit 1
 fi
